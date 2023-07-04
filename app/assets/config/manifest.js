df0f5aa0ead1d68f4b28fd4a2b5418c3057a2b5a
AdrieneJas <adrianajasielewicz@gmail.com>
2023-07-04 12:33:51 +0000

    Adding navigation

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= require bootstrap-sprockets
+//= require bootstrap
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
